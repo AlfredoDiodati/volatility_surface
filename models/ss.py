db1f5234db7a3a29b38f3f7e95d8bf846f0d047d
Alfredo Diodati <diodatialfredo@gmail.com>
2026-02-14 16:55:16 +0100

    implemented simulation

--- models/ss.py
+++ models/ss.py
@@ -1,18 +1,14 @@
 import numpy as np
-from _kalman._filter import _filter as kfilter
-from _kalman._filter import _loglikelihood
+from models._kalman import _filter as kfilter
+from models._kalman import _loglikelihood
 
-def _dynamics(_y, _a, _P, params, _Z, bt, H, identity_mat, Q, idx)->dict:
+def _dynamics(y, _a, _P, params, _Z, bt, _H, identity_mat, _Q, idx)->dict:
     p = bt.shape[0]
+    Q = params["Q_param"]
+    H = params["H_param"]
     Mt = params["covariates"][idx * p : (1 + idx)*p, :]
     bar_beta = params["bar_beta"]
     B = params["B"]
-    Z = Mt
+    Z = np.where(np.isnan(y), 0.0, Mt)
     T = (identity_mat - B) @ bar_beta + B @ bt
     return Z, T, H, identity_mat, Q
-
-def fit(data: np.ndarray, params: dict, covariates:np.ndarray):
-    params["covariates"] = covariates
-    def _criterion(params):
-        kf = kfilter(data, _dynamics, params)
-        return _loglikelihood(kf)
