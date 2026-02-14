cadbc2bf37f9806f5c79f78248fa1534549487a8
Alfredo Diodati <diodatialfredo@gmail.com>
2026-02-14 14:31:30 +0100

    simplified structure

--- _backend/_np.py
+++ _backend/_np.py
@@ -1,11 +1,6 @@
 """Generates Backend classes to make the code work for both numpy and JAX"""
 
 import numpy as np
-from typing import Protocol
-
-class ndarray(Protocol):
-    shape: tuple
-    ndim: int
 
 import numpy as np
 
@@ -49,28 +44,3 @@ def numpy_scan(f, carry, xs):
         carry, y = f(carry, x)
         _write(ys, i, y)
     return carry, ys
-
-class Backend(Protocol):
-    xp = ...
-    scan = ...
-
-class NumpyBackend(Backend):
-    xp = np
-    scan = staticmethod(numpy_scan)
-
-class JaxBackend(Backend):
-    import jax.numpy as xp
-    from jax.lax import scan
-
-def _set_backend(backend:Backend) -> None:
-    """Sets the linear algebra backend for the code, if nothing is selected it defaults to numpy.
-
-    Args:
-        backend (ModuleType): linear algebra module with numpy syntax.
-
-    Possible alternatives are numpy and jax. CUPy may also work but would require further implementation
-    """
-    global xp
-    global scan
-    xp = backend.xp
-    scan = backend.scan
--- _kalman/_filter.py
+++ _kalman/_filter.py
@@ -6,37 +6,37 @@ Implementation and notation based on:
 Durbin, J. and Siem Jan Koopman (2012). Time Series Analysis by State Space Methods. OUP Oxford.
 """
 
-import filter._backend as _backend
-from filter._backend import ndarray
-xp = _backend.Backend.xp
-scan = _backend.Backend.scan
+from _backend._np import scan
+import numpy as np
 
-def _filter(data: ndarray, dynamics:callable, carry0 = (0.0, 10e7))->dict:
+def _filter(data: np.ndarray, dynamics:callable, params:dict, carry0)->dict:
     """Kalman Filter implementation
 
     Args:
         data (ndarray): data in compatible ndarray
         dynamics (callable): function that specifies Zt, Tt, Rt and Qt
+        params (dict): parameters of the model
         carry0 (tuple[float, float]): initial state prediction and variance
 
     """
 
     def _step(carry, yt):
         """we carry forward at and Pt for prediction and filter, sum """
-        at_pred, Pt_pred = carry
-        Zt, Tt, Ht, Rt, Qt = dynamics(yt, at_pred, Pt_pred)
+        at_pred, Pt_pred, Zt, Tt, Ht, Rt, Qt, idx= carry
+        Zt, Tt, Ht, Rt, Qt = dynamics(yt, at_pred, Pt_pred, params, Zt, Tt, Ht, Rt, Qt, idx)
         vt = yt - Zt @ at_pred
         ZP = Zt @ Pt_pred
         Ft = ZP @ Zt.T + Ht
-        L = xp.linalg.cholesky(Ft)
-        tmp = xp.linalg.solve(L, ZP)
-        Kt = xp.linalg.solve(L.T, tmp).T
+        L = np.linalg.cholesky(Ft)
+        tmp = np.linalg.solve(L, ZP)
+        Kt = np.linalg.solve(L.T, tmp).T
         at_filt = Tt @ at_pred + Kt @ vt
         Ptp1 = Tt @ Pt_pred @ Tt.T + Rt @ Qt @ Rt.T - Kt @ ZP
-        Linv_v = xp.linalg.solve(L, vt)
+        Linv_v = np.linalg.solve(L, vt)
         quad_t = Linv_v.T @ Linv_v
-        logdet_t = 2.0 * xp.sum(xp.log(xp.diag(L)))
-        new_carry = (at_filt, Ptp1)
+        logdet_t = 2.0 * np.sum(np.log(np.diag(L)))
+        idx +=1
+        new_carry = (at_filt, Ptp1, Zt, Tt, Ht, Rt, Qt, idx)
         store_timet = {"a": at_filt,"P": Ptp1,"Z": Zt,"T": Tt,
             "H": Ht,"R": Rt,"Q": Qt,"v": vt,
             "F": Ft,"logdetF": logdet_t,"quad": quad_t,}
