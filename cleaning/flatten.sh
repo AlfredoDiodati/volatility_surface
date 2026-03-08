411e8058adcf15dae1d4001f254537e314662efd
Alfredo Diodati <diodatialfredo@gmail.com>
2026-03-08 10:30:35 +0100

    added pipeline script

---
+++ cleaning/flatten.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+target="${1:-.}"
+
+find "$target" -mindepth 2 -type f | while read -r file; do
+    filename=$(basename "$file")
+    dest="$target/$filename"
+
+    if [ -e "$dest" ]; then
+        base="${filename%.*}"
+        ext="${filename##*.}"
+        [ "$base" = "$filename" ] && ext="" || ext=".$ext"
+        counter=1
+        while [ -e "$target/${base}_${counter}${ext}" ]; do
+            ((counter++))
+        done
+        dest="$target/${base}_${counter}${ext}"
+    fi
+
+    mv "$file" "$dest"
+done
+
+find "$target" -mindepth 1 -type d -empty -delete
